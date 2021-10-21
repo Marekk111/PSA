4ca14ef281c4a33842fd6ca37f06396e51a8b28e
Marekk111 <cunderlikmarek0@gmail.com>
2021-10-21 09:42:48 +0200

    Cvicenie4

--- Cv4-client.py
+++ Cv4-client.py
@@ -1,12 +1,75 @@
 #!/usr/bin/env python3
 
 import socket
+from enum import IntEnum
+import json
 
-ADRESS = "127.0.0.1"
-PORT = 9999
+ADRESS = "0.0.0.0"
+PORT = 9998
+
+class Sprava:
+    def __init__(self, paOd, paKomu, paOperacia, paText):
+        self.aOd = paOd
+        self.aKomu = paKomu
+        self.aOperacia = paOperacia
+        self.aText = paText
+    
+    @staticmethod
+    def jsonParser(obj):
+        return Sprava(obj['aOd'], obj['aKomu'], obj['aOperacia'], obj['aText'])
+        
+
+class Operacia(IntEnum):
+    LOGIN = 1
+    EXIT = 2
+    USERS = 3
+
+def napoveda():
+    print("***Napoveda")
+    print("  \q Ukonci program")
+    print("  \l Vypis pouzivatelov")
+    print("  \h Vypis napovedu")
+    print("Spravu posielame v tvare nick;text")
 
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ADRESS, PORT))
-    sprava = input("Zadaj spravu: ")
-    sock.send(sprava.encode())
+    
+    print("Vita vas CHAT client.")
+    nick = input("Zadajte vas nick: ")
+    napoveda()
+    # prihlasenie na server (pre zoznam pouzivatelov)
+    sprava = Sprava(nick, '', Operacia.LOGIN, '')
+    jsonStr = json.dumps(sprava.__dict__)
+    sock.send(jsonStr.encode())    
+    
+    while(True):
+
+        prikaz = input("Zadajte prikaz/spravu: ")
+
+        if prikaz[0] == "\\":
+            # mam specialny prikaz
+            if prikaz[1] == "q":
+                # ukoncujem program
+                sprava = Sprava(nick, '', Operacia.EXIT, '')
+                jsonStr = json.dumps(sprava.__dict__)
+                sock.send(jsonStr.encode())
+                sock.close()
+                exit(0)
+            if prikaz[1] == "h":
+                napoveda()
+                continue
+            if prikaz[1] == "l":
+                sprava = Sprava(nick, '', Operacia.USERS, '')
+                jsonStr = json.dumps(sprava.__dict__)
+                sock.send(jsonStr.encode())
+                data = sock.recv(1500)
+                json.loads(data.decode(), object_hook=Sprava.jsonParser())
+                print("Zoznam pouzivatelov: " + Sprava.aText)
+                continue
+        
+        zoznam = prikaz.split(";", 1)
+        sprava = Sprava(nick, zoznam[0], '', zoznam[1])
+        jsonStr = json.dumps(sprava.__dict__)
+        sock.send(jsonStr.encode())
+            
--- Cv4-server.py
+++ Cv4-server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import socket
-ADRESS = "127.0.0.1"
-PORT = 9999
+ADRESS = "0.0.0.0"
+PORT = 9998
 
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,7 +10,8 @@ if __name__ == "__main__":
 
     while(True):
         (clientSock, clientAddr) = sock.accept()
-        sprava = clientSock.recv(1500)
-        print("Sprava od {}:{} {}".format(clientAddr[0], clientAddr[1], sprava))
-        clientSock.close()
+        while(True):
+            sprava = clientSock.recv(1500)
+            print("Sprava od {}:{} {}".format(clientAddr[0], clientAddr[1], sprava))
+            clientSock.close()
         
